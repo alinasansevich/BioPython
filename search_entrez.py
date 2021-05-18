35f25f407e7eae7263486e8d04107dd038e4ae53
Alina Sansevich <alinasansevich@gmail.com>
2021-05-18 14:27:42 -0500

    CLI ready.

--- search_entrez.py
+++ search_entrez.py
@@ -12,8 +12,6 @@ Resources:
     https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESearch
     http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec139
     http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec%3Aefetch
-
-
 """
 
 import os
@@ -23,26 +21,15 @@ from Bio import SeqIO
 
 Entrez.email = "alinasansevich@gmail.com"     # Always tell NCBI who you are
 
-# Create the top-level parser
+# Create parser
 parser = argparse.ArgumentParser(description='Search the Entrez database with a gene sequence accession number.')
-parser.add_argument('-a', '--accession', type=str, metavar='', required=True, help='Gene sequence accession number')
-subparsers = parser.add_subparsers(help='Choose file format (FASTA or GenBank full description) \n and action to perform (Print to Terminal or Save to file)')
-
-# Create the parser for the "choose format" command
-parser_f = subparsers.add_parser('file_format', help='Choose file format (FASTA or GenBank full description)')
-parser_f.add_argument('-f', '--fasta', action='store', required=True, help='FASTA file format')
-parser_f.add_argument('-g', '--genbank', action='store', required=True, help='GenBank full description file')
-
-# create the parser for the "choose action" command
-parser_d = subparsers.add_parser('display', help='Choose how to display data: Print to Terminal or Save to file')
-parser_d.add_argument('-p', '--print', action='store', required=True, help='Print in Terminal')
-parser_d.add_argument('-s', '--save', action='store', required=True, help='Save to file (.txt)')
-
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-q', '--quiet', action='store_true', help='print quiet')
-group.add_argument('-v', '--verbose', action='store_true', help='print verbose')
+# Add positional argument
+parser.add_argument('accession_num', metavar='accession_n', help='Gene sequence accession number')
+# Add optional arguments
+parser.add_argument('-f', '--fasta', action='store_true', help='Save sequence as FASTA file format (.fasta)')
+parser.add_argument('-p', '--print', action='store_true', help='Print sequence in FASTA format in the Terminal')
+parser.add_argument('-s', '--save', action='store_true', help='Save GenBank full description to file (.txt)')
 args = parser.parse_args()
-
 
 def get_genbank_file(accession_num):
     """
@@ -53,100 +40,60 @@ def get_genbank_file(accession_num):
         Gene sequence accession number.
     """
     handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="gb", retmode="text")
-   
-    if args.print:
-        print(handle.read())     # print on terminal
-    elif args.save:
-        f = open("demofile.txt", "w")    # save to file
-        f.write(handle.read())
-        f.close()
+    return handle.read()
+
+
+def print_genbank(genbank):
+    # save to file
+    filename = '{}.txt'.format(args.accession_num)
+    f = open(filename, "w")
+    f.write(genbank)
+    f.close()
 
 
 def get_fasta(accession_num):
     """
-    Fetch sequence in FASTA format for accession_num and 
-    print to Terminal or Save to file as .fasta
+    Fetch sequence in FASTA format for accession_num and save to file as .fasta
     
     accession_num : str
         Gene sequence accession number.
     """
+    handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta", retmode="text")
+    # save to file
+    filename = '{}.fasta'.format(accession_num)
+    out_handle = open(filename, "w")
+    in_handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta")
+    record = SeqIO.parse(in_handle, "fasta")
+    SeqIO.write(record, out_handle, "fasta")
+    in_handle.close()
+    out_handle.close()
+
+
+def print_fasta(accession_num):
+    """
+    Fetch sequence in FASTA format for accession_num and print to Terminal
     
-    if args.print:
-        handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta", retmode="text")
-        print(handle.read())   # print on terminal
-    elif args.save:
-        filename = '{}.fasta'.format(accession_num)
-        out_handle = open(filename, "w")
-        in_handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta")
-        record = SeqIO.parse(in_handle, "fasta")
-        SeqIO.write(record, out_handle, "fasta")
-        in_handle.close()
-        out_handle.close()
+    accession_num : str
+        Gene sequence accession number.
+    """
+    handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta", retmode="text")
+    # print on terminal
+    return handle.read()
+
 
 
 if __name__ == '__main__':
-    if args.quiet:
-        if args.fasta:
-            get_fasta(args.accession)
-            print('Done.')
-        elif args.genbank:
-            get_genbank_file(args.accession)
-            print('Done.')
-    elif args.verbose:
-        if args.fasta:
-            print("Fetching data...")
-            get_fasta(args.accession)
-            print('Done.')
-        elif args.genbank:
-            print("Fetching data...")
-            get_genbank_file(args.accession)
-            print('Done.')
+    print('\n\n')
+    genbank = get_genbank_file(args.accession_num)
+    print(genbank)
+    if args.save:
+        print_genbank(genbank)
+    if args.fasta:
+        get_fasta(args.accession_num)
+    if args.print:
+        fasta = print_fasta(args.accession_num)
+        print(fasta)
         
-            
-        
-
-
-
-
-
-
-
-# https://www.reddit.com/r/learnpython/comments/69okp4/argparse_one_argument_mutually_exclusive_to_two/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # NM_119948.4
-
-# https://stackoverflow.com/questions/17909294/python-argparse-mutual-exclusive-group
-# create the top-level parser
-# parser = argparse.ArgumentParser(prog='PROG')
-# parser.add_argument('--foo', action='store_true', help='help for foo arg.')
-# subparsers = parser.add_subparsers(help='help for subcommand')
-
-# create the parser for the "command_1" command
-# parser_a = subparsers.add_parser('command_1', help='command_1 help')
-# parser_a.add_argument('a', type=str, help='help for bar, positional')
-
-# create the parser for the "command_2" command
-# parser_b = subparsers.add_parser('command_2', help='help for command_2')
-# parser_b.add_argument('-b', type=str, help='help for b')
-# parser_b.add_argument('-c', type=str, action='store', default='', help='test')
