c764390adc576732ec46a8990eb5c6f1787233f1
Alina Sansevich <alinasansevich@gmail.com>
2021-05-19 12:50:50 -0500

    Added try except for HTTPError.

--- search_entrez.py
+++ search_entrez.py
@@ -5,7 +5,7 @@ Created on Mon May 17 10:04:43 2021
 
 @author: alina
 
-This program will search the Entrez database for a gene, using the accession number provided by the user.
+This program will search the Entrez database for a gene/protein, using the accession number provided by the user.
 The user may choose the results format (FASTA or GenBank full description file).
 
 Resources:
@@ -14,7 +14,7 @@ Resources:
     http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec%3Aefetch
 """
 
-import os
+import urllib
 import argparse
 from Bio import Entrez
 from Bio import SeqIO
@@ -31,7 +31,7 @@ parser.add_argument('-p', '--print', action='store_true', help='Print sequence i
 parser.add_argument('-s', '--save', action='store_true', help='Save GenBank full description to file (.txt)')
 args = parser.parse_args()
 
-def get_genbank_file(accession_num):
+def get_genbank_file(accession_num):  # add parameter 'db', that let's the user choose nucleotide or protein
     """
     Fetch GenBank full description data for accession_num and 
     print to Terminal or Save to file as .txt.
@@ -44,22 +44,22 @@ def get_genbank_file(accession_num):
 
 
 def print_genbank(genbank):
-    # save to file
+    """
+    Save to file (.txt) the str value returned by a call to get_genbank_file.
+    """
     filename = '{}.txt'.format(args.accession_num)
     f = open(filename, "w")
     f.write(genbank)
     f.close()
 
 
-def get_fasta(accession_num):
+def get_fasta(accession_num): # add parameter 'db', that let's the user choose nucleotide or protein
     """
     Fetch sequence in FASTA format for accession_num and save to file as .fasta
     
     accession_num : str
         Gene sequence accession number.
     """
-    handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta", retmode="text")
-    # save to file
     filename = '{}.fasta'.format(accession_num)
     out_handle = open(filename, "w")
     in_handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta")
@@ -69,7 +69,7 @@ def get_fasta(accession_num):
     out_handle.close()
 
 
-def print_fasta(accession_num):
+def print_fasta(accession_num):  # add parameter 'db', that let's the user choose nucleotide or protein
     """
     Fetch sequence in FASTA format for accession_num and print to Terminal
     
@@ -77,22 +77,27 @@ def print_fasta(accession_num):
         Gene sequence accession number.
     """
     handle = Entrez.efetch(db="nucleotide", id=accession_num, rettype="fasta", retmode="text")
-    # print on terminal
     return handle.read()
 
 
 
 if __name__ == '__main__':
-    print('\n\n')
-    genbank = get_genbank_file(args.accession_num)
-    print(genbank)
-    if args.save:
-        print_genbank(genbank)
-    if args.fasta:
-        get_fasta(args.accession_num)
-    if args.print:
-        fasta = print_fasta(args.accession_num)
-        print(fasta)
+    print("The Entrez database requires that you provide your email address to make the request.")
+    user_email = input("Please enter your email address: ")
+    try:
+        print('\n\n')
+        genbank = get_genbank_file(args.accession_num)
+        print(genbank)
+        if args.save:
+            print_genbank(genbank)
+        if args.fasta:
+            get_fasta(args.accession_num)
+        if args.print:
+            fasta = print_fasta(args.accession_num)
+            print(fasta)
+    except urllib.error.HTTPError:
+        print("HTTP Error 400: Bad Request")
+        print("Invalid accession number provided.")
         
 
 
